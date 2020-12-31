1c393e21a95e0d6ab06f597c78adaf8ba319ef6c
dmitrijsidelnikov <kiberline@yandex.ru>
2020-12-31 13:32:44 +0200

    Thymeleaf fragments + fix typo + pom

---
+++ MyBookShop/src/test/java/ru/sd/MyBookShop/MyBookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sd.MyBookShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
